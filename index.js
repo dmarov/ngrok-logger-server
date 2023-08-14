59af5ddc9d5c88375774aa27b645bcd9e13f3eb1
Dmitry Marov <d.marov94@gmail.com>
2023-08-14 15:53:56 +0300

    cookie test

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async ctx => {
-  ctx.body = 'Hello World';
+  ctx.body = {
+    message: "Hello World",
+    obj: { 
+      field1: "a",
+      field2: "b",
+      field3: "b",
+      field4: "b",
+      field5: "b",
+      field6: "b",
+      field7: "b",
+      field8: "b",
+      field9: "b",
+      field10: "b",
+      field11: "b",
+    },
+  };
+
+  ctx.set('X-My-Custom-Header', '123abc');
+
+  ctx.cookies.set('X-My-Custom-Cookie', 'valueOfCookie');
+
   console.log(ctx.request);
 });
 
